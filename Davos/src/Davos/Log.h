4835d2b110963b5439f0b1d982797251e796fb8a
DavidTello1 <davidtellop@gmail.com>
2025-08-27 18:49:29 +0200

    Log

---
+++ Davos/src/Davos/Log.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "Core.h"
+
+#include "spdlog/spdlog.h"
+#include <memory>
+
+namespace Davos {
+
+	class DAVOS_API Log
+	{
+	public:
+		static void Init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	};
+
+}
+
+// Core Log Macros
+#define DVS_CORE_TRACE(...)		::Davos::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define DVS_CORE_INFO(...)		::Davos::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define DVS_CORE_WARN(...)		::Davos::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define DVS_CORE_ERROR(...)		::Davos::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define DVS_CORE_CRITICAL(...)	::Davos::Log::GetCoreLogger()->critical(__VA_ARGS__)
+
+// Client Log Macros
+#define DVS_TRACE(...)			::Davos::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define DVS_INFO(...)			::Davos::Log::GetClientLogger()->info(__VA_ARGS__)
+#define DVS_WARN(...)			::Davos::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define DVS_ERROR(...)			::Davos::Log::GetClientLogger()->error(__VA_ARGS__)
+#define DVS_CRITICAL(...)		::Davos::Log::GetClientLogger()->critical(__VA_ARGS__)
