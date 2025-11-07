5e7e30f287daed2d355c62f664ee1d06de4546f8
DavidTello1 <davidtellop@gmail.com>
2025-11-07 23:59:02 +0100

    Editor Assets

---
+++ DavosEditor/assets/shaders/Texture.glsl
@@ -0,0 +1,33 @@
+// --- TEXTURE SHADER ---
+
+#type vertex
+#version 330 core
+
+layout(location = 0) in vec3 a_Position;
+layout(location = 1) in vec2 a_TexCoords;
+
+out vec2 v_TexCoords;
+
+uniform mat4 u_Model;
+uniform mat4 u_ViewProjection;
+
+void main()
+{
+	v_TexCoords = a_TexCoords;
+	gl_Position =  u_ViewProjection * vec4(a_Position, 1.0);
+}
+
+
+#type fragment
+#version 330 core
+
+layout(location = 0) out vec4 o_Color;
+
+in vec2 v_TexCoords;
+
+uniform sampler2D u_Texture;
+
+void main()
+{
+	o_Color = texture(u_Texture, v_TexCoords);
+}
